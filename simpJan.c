a3f33d10c5caa62751e474571f8b230d4ccb9fad
ygorcf <ygorcruzfelix@gmail.com>
2017-01-06 20:26:16 -0200

    Atualizada arquivo completo da biblioteca

--- simpJan.c
+++ simpJan.c
@@ -49,6 +49,163 @@ typedef HWND sj_Janela;
 
 #endif // #ifndef SIMP_JAN_TIPOS_BASICOS_H
 
+#ifndef SIMP_JAN_STRING_H
+#define SIMP_JAN_STRING_H
+
+#include <string.h>
+
+int sj_strIgual(char *str1, char *str2);
+char *sj_strStrip(char *_str);
+char *sj_strStripCaracs(char *_str, char *caracs);
+char *sj_strStripCarac(char *_str, char carac);
+char *sj_strCopia(char *_str);
+
+#ifndef SIMP_JAN_STRING_C
+#define SIMP_JAN_STRING_C
+
+
+int sj_strIgual(char *str1, char *str2) {
+  return !strcmp(str1, str2);
+}
+
+char *sj_strStrip(char *_str) {
+  return sj_strStripCaracs(_str, "\t\r\n \"'");
+}
+
+char *sj_strStripCaracs(char *_str, char *caracs) {
+  int ini = 0, fim = strlen(_str) - 1;
+  char *c = _str;
+  while (*c != '\0') {
+    if (strchr(caracs, *c++) != NULL)
+      ini++;
+    else
+      break;
+  }
+  c = _str + fim;
+  while (c != _str) {
+    if (strchr(caracs, *c--) != NULL)
+      fim--;
+    else
+      break;
+  }
+  char *ret = alocas(char, fim + 1);
+  if (ret == NULL)
+    return NULL;
+  if (strncpy(ret, _str + ini, fim) == NULL) {
+    free(ret);
+    return NULL;
+  }
+  ret[fim] = '\0';
+  return ret;
+}
+
+char *sj_strStripCarac(char *_str, char carac) {
+  char *t = alocas(char, 2);
+  if (t == NULL)
+    return NULL;
+  t[0] = carac;
+  t[1] = '\0';
+  char *ret = sj_strStripCaracs(_str, t);
+  free(t);
+  return ret;
+}
+
+char *sj_strCopia(char *_str) {
+  int tam = strlen(_str);
+  char *ret = malloc(tam * sizeof(char));
+  if (ret == NULL)
+    return NULL;
+  if (strcpy(ret, _str) == NULL) {
+    free(ret);
+    return NULL;
+  }
+  return ret;
+}
+
+#endif // #ifndef SIMP_JAN_STRING_C
+
+#endif // #ifndef SIMP_JAN_STRING_H
+
+#ifndef SIMP_JAN_INTERPRETADOR_H
+#define SIMP_JAN_INTERPRETADOR_H
+
+#include <string.h>
+
+typedef enum {
+  SJ_ID_JANELA
+} idsSj;
+
+idsSj *obterTodosIds(char *_str);
+char *pularCaracs(char *_str, char *caracs);
+char *localPrimeiro(char *_str, char *caracsProcurar);
+
+#ifndef SIMP_JAN_INTERPRETADOR_C
+#define SIMP_JAN_INTERPRETADOR_C
+
+
+// copiar toda string
+
+// pegar todas palavras
+//  loop
+//   strip na string
+//   pegar tamanho da palavra
+//   copiar palavra
+//   adicionar no array
+//   move ponteiro str
+// transformar palavras em identificadores
+// retornar array de identificadores
+idsSj *obterTodosIds(char *_str) {
+  char *copiaStr = sj_strCopia(_str);
+  if (copiaStr == NULL) return NULL;
+  char *caracsPular = "  :=;'\"\r\t\n";
+  char *temp = NULL, *add = NULL;
+  int tam = 0;
+  while (*copiaStr != '\0') {
+    copiaStr = pularCaracs(copiaStr, caracsPular);
+    if (copiaStr == NULL)
+      return NULL;
+    temp = localPrimeiro(copiaStr, caracsPular);
+    tam = temp - copiaStr;
+    add = (char *)malloc((tam + 1) * sizeof(char));
+    if (add == NULL) {
+      free(copiaStr);
+      return NULL;
+    }
+    if (strncpy(add, copiaStr, tam) == NULL) {
+      free(copiaStr);
+      free(add);
+      return NULL;
+    }
+    copiaStr = temp;
+    printf("|%s|\n", add);
+  }
+  return NULL;
+}
+
+char *pularCaracs(char *_str, char *caracs) {
+if (_str == NULL) return NULL;
+  char *t = _str - 1;
+  while (*++t != '\0') {
+    if (strchr(caracs, *t) == NULL)
+      break;
+  }
+  return t;
+}
+
+char *localPrimeiro(char *_str, char *caracsProcurar) {
+if (_str == NULL) return NULL;
+  char *t = _str - 1;
+  while (*++t != '\0') {
+    if (strchr(caracsProcurar, *t) != NULL)
+      break;
+  }
+  return t;
+}
+
+#endif // #ifndef SIMP_JAN_INTERPRETADOR_C
+
+#endif // #ifndef SIMP_JAN_INTERPRETADOR_H
+
 #ifndef SIMP_JAN_RETORNO_BASE_H
 #define SIMP_JAN_RETORNO_BASE_H
 
