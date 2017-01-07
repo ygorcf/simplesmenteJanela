194fbb259868556f00f26361aa0b4faad060747a
ygorcf <ygorcruzfelix@gmail.com>
2017-01-07 18:38:36 -0200

    Corrigida funcao de retornar ids, criadas funcoes pra verificar id e comecada funcao para criar as janelas apartir de uma string com as configuracoes/opcoes/propriedades das janelas

--- src/interpretador/interpretador.c
+++ src/interpretador/interpretador.c
@@ -15,6 +15,8 @@
 // transformar palavras em identificadores
 // retornar array de identificadores
 idsSj *obterTodosIds(char *_str) {
+  idsSj *ret = NULL, *retTemp;
+  int qtdId = 1;
   char *copiaStr = sj_strCopia(_str);
   if (copiaStr == NULL) return NULL;
   char *caracsPular = "  :=;'\"\r\t\n";
@@ -37,9 +39,17 @@ idsSj *obterTodosIds(char *_str) {
       return NULL;
     }
     copiaStr = temp;
-    printf("|%s|\n", add);
+    retTemp = ret;
+    retTemp = (idsSj *)realloc(ret, (qtdId + 1) * sizeof(idsSj));
+    if (retTemp == NULL) {
+      free(ret);
+      return NULL;
+    }
+    ret = retTemp;
+    ret[qtdId - 1] = obterId(add);
+    ret[qtdId++] = SJ_ID_NULO;
   }
-  return NULL;
+  return ret;
 }
 
 char *pularCaracs(char *_str, char *caracs) {
@@ -60,6 +70,111 @@ if (_str == NULL) return NULL;
       break;
   }
   return t;
+}
+
+idsSj obterId(char *_str) {
+  if (sj_strEInt(_str))
+    return SJ_ID_INT;
+  if (sj_strIgual(_str, "janela"))
+    return SJ_ID_JANELA;
+  if (sj_strIgual(_str, "altura"))
+    return SJ_ID_ALTURA;
+  if (sj_strIgual(_str, "largura"))
+    return SJ_ID_LARGURA;
+}
+
+void printId(idsSj id) {
+  switch (id) {
+    case SJ_ID_JANELA:
+      printf("Janela");
+      break;
+    case SJ_ID_INT:
+      printf("Inteiro");
+      break;
+    case SJ_ID_ALTURA:
+      printf("Altura");
+      break;
+    case SJ_ID_LARGURA:
+      printf("Largura");
+      break;
+    default:
+      printf("indefinido");
+  }
+  printf("\n");
+}
+
+int idEPropriedade(idsSj id) {
+  switch (id) {
+    case SJ_ID_ALTURA:
+      return 1;
+    case SJ_ID_LARGURA:
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+int idEValor(idsSj id) {
+  switch (id) {
+    case SJ_ID_INT:
+      return 1;
+    default:
+      return 0;
+  }
+}
+
+sj_Janela *obterJanelas(sj_Id idProcesso, char *configuracoes) {
+  char *classe = NULL, *titulo = NULL;
+  int larg, alt, esperadoValor = 0, *valorAlterar = NULL, qtdjan = 1;
+  sj_Janela *ret = NULL, *retTemp;
+  idsSj *a = obterTodosIds(configuracoes);
+  /*ConfiguracoesJanela *conf = criaConfiguracoesJanela(idProcesso, "Window", c, 350, 250);
+  sj_Janela jan = criaJanela(conf);
+  executaJanela(jan);*/
+  while (*a != SJ_ID_NULO) {
+    if (esperadoValor && idEValor(*a)) {
+      *valorAlterar = 100;
+      esperadoValor = 0;
+    } else if (idEPropriedade(*a)) {
+      esperadoValor = 1;
+      switch (*a) {
+        case SJ_ID_ALTURA:
+          valorAlterar = &alt;
+          break;
+        case SJ_ID_LARGURA:
+          valorAlterar = &larg;
+          break;
+        default :
+          return NULL;
+      }
+    } else {
+      larg = 350;
+      alt = 250;
+      esperadoValor = 0;
+      if (titulo != NULL) free(titulo);
+      titulo = (char *)malloc(7 * sizeof(char));
+      if (titulo == NULL) return NULL;
+      strcpy(titulo, "Janela\0");
+      switch (*a) {
+        case SJ_ID_JANELA:
+          classe = "Window";
+          break;
+        default :
+          return NULL;
+      }
+    }
+    retTemp = ret;
+    retTemp = (sj_Janela *)realloc(ret, (qtdjan + 1) * sizeof(sj_Janela));
+    if (retTemp == NULL) {
+      free(ret);
+      return NULL;
+    }
+    ret = retTemp;
+    ConfiguracoesJanela *conf = criaConfiguracoesJanela(idProcesso, classe, titulo, larg, alt);
+    ret[qtdjan - 1] = criaJanela(conf);
+    ret[qtdjan++] = NULL;
+  }
+  return ret;
 }
 
 #endif // #ifndef SIMP_JAN_INTERPRETADOR_C
--- src/interpretador/interpretador.h
+++ src/interpretador/interpretador.h
@@ -4,12 +4,21 @@
 #include <string.h>
 
 typedef enum {
-  SJ_ID_JANELA
+  SJ_ID_INDEFINIDO,
+  SJ_ID_NULO,
+  SJ_ID_JANELA,
+  SJ_ID_INT,
+  SJ_ID_ALTURA,
+  SJ_ID_LARGURA
 } idsSj;
 
 idsSj *obterTodosIds(char *_str);
 char *pularCaracs(char *_str, char *caracs);
 char *localPrimeiro(char *_str, char *caracsProcurar);
+idsSj obterId(char *_str);
+void printId(idsSj id);
+int idEPropriedade(idsSj id);
+int idEValor(idsSj id);
 
 #include "interpretador.c"
 
