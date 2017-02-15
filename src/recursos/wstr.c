78e22ccda667e7100465407fa8d10ab8e892c84b
Ygor Cruz <ygorcruzfelix@gmail.com>
2017-02-14 23:54:39 -0200

    Corrigido bug de alocacao dos titulos'

--- src/recursos/wstr.c
+++ src/recursos/wstr.c
@@ -5,7 +5,7 @@
 #include "alocacao.h"
 
 wchar_t *strToWstr(char *_str) {
-  int len = strlen(_str);
+  int len = strlen(_str) + 1;
   wchar_t *ret;
   ret = alocas(wchar_t, len);
   if (ret != NULL) {
